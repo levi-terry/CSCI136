5595cb0c69fcbd570c29c2ca4ad2f839837ed03a
Levi Terry <ldterry14@gmail.com>
2019-01-28 20:50:26 -0700

    Created recursion_begin.py file

---
+++ hw_30JAN/recursion_begin.py
@@ -0,0 +1,8 @@
+# Author: LDT
+# Date: 27JAN2019
+# Title: recursion_begin.py
+# Purpose: This program implements two functions.
+# The first function accepts an int array as a parameter
+# and returns the sum of the array using recursion. The
+# second function validates nested parentheses oriented
+# correctly, such as (), (()()), and so on using recursion
