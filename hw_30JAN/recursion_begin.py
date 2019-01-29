637cf488ad27a91241aa7e3560ca8c9190720b46
Levi Terry <ldterry14@gmail.com>
2019-01-28 21:45:34 -0700

    Gave barebones to recursion_begin.py Added test cases Still need to add functions

--- hw_30JAN/recursion_begin.py
+++ hw_30JAN/recursion_begin.py
@@ -6,3 +6,44 @@
 # and returns the sum of the array using recursion. The
 # second function validates nested parentheses oriented
 # correctly, such as (), (()()), and so on using recursion
+
+
+# Int sum w/recursion function
+def int_sum(array):  # FIXME: Add the code
+    pass
+
+
+# Parentheses function recursion
+def parentheses_nesting(string):  # FIXME: Add the code, what to return?
+    pass
+
+
+# Code testing
+if __name__ == "__main__":
+
+    # Test int_sum function
+    intArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    testArray = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+    print("Testing int_sum function...")
+    print("Results should read [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]")
+    print(int_sum(intArray))
+    if int_sum(intArray) == testArray:
+        print("Success!")
+    else:
+        print("Need to fix int_sum function...")
+
+    # Test parentheses_nesting function
+    paren1 = "()"  # Good
+    paren2 = "()()"  # Good
+    paren3 = "(()())"  # Good
+    paren4 = "((()()()))"  # Good
+    paren5 = ")("  # Bad
+    paren6 = "(()))())"  # Bad
+    print("Testing parentheses_nesting function...")
+    print("Results TBD")  # FIXME: Figure out what results should look like after writing code
+    print(parentheses_nesting(paren1))
+    print(parentheses_nesting(paren2))
+    print(parentheses_nesting(paren3))
+    print(parentheses_nesting(paren4))
+    print(parentheses_nesting(paren5))
+    print(parentheses_nesting(paren6))
