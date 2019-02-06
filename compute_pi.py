77a164a1351f4f58795dcd8b7a5868443867b9d5
Levi Terry <ldterry14@gmail.com>
2019-02-05 18:33:58 -0700

    Added barebones of 6FEB hw and compute_pi

---
+++ compute_pi.py
@@ -0,0 +1,14 @@
+# Author: LDT
+# Date: 05FEB2019
+# Title: compute_pi.py
+# Purpose: This program takes a user-determined number of
+# random x, y coordinates to roughly approximate the value of pi.
+
+
+def pi():
+    pass  # FIXME: Write code
+
+
+# Test Code
+if __name__ == "__main__":
+    pass  # FIXME: Create test code
---
+++ hw_6FEB/hw.py
@@ -0,0 +1,32 @@
+# Author: LDT
+# Date: 05FEB2019
+# Title: hw.py
+# Purpose: Various functions as defined.
+
+
+# returns the unique prime factors of a number as a set, e.g. f(8) = {2}, f(9) = {3}, f(10) = {2, 5}, f(12) = {2, 3}
+def prime_set(number):
+    pass  # FIXME: Write the code
+
+
+# returns the unique prime factors as a map of the factor to how many times it is used,
+# e.g. f(8) = {2: 3}, f(9) = {3: 2}, f(10) = {2: 1, 5: 1}
+def prime_map(number):
+    pass  # FIXME: Write the code
+
+
+def whatever():
+    pass  # FIXME: 3.2.6
+
+
+def whichever():
+    pass  # FIXME: 3.2.7
+
+
+def whenever():
+    pass  # FIXME: 3.2.10
+
+
+# Test Code
+if __name__ == "__main__":
+    pass  # FIXME: Write tests
