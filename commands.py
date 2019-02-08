bd8e8f2cd639b2d1f9c80fe68676d77f12789972
Levi Terry <ldterry14@gmail.com>
2019-02-08 14:49:56 -0700

    Edits to commands.py

--- commands.py
+++ commands.py
@@ -12,7 +12,28 @@ elif sys.argv[1] == "head":
             break
 
 elif sys.argv[1] == "tail":
-    print("tail")
+    stuff = []
+    for line in sys.stdin:
+        stuff.append(line)
+    print(stuff[-4], end='')
+    print(stuff[-3], end='')
+    print(stuff[-2], end='')
+    print(stuff[-1], end='')
+
+elif sys.argv[1] == "unique":
+    newSet = set()
+    for line in sys.stdin:
+        if line in newSet:
+            continue
+        else:
+            newSet.add(line)
+    print(newSet)
+
+elif sys.argv[1] == "count":
+    pass
+
+elif sys.argv[1] == "sort":
+    pass
 
 else:
     print("Invalid command")
