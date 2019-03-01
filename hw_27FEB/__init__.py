26fae81a9e250e232dfeb01bcfba5fe96d69f51f
Levi Terry <ldterry14@gmail.com>
2019-03-01 08:33:20 -0700

    Added some comments

--- hw_27FEB/__init__.py
+++ hw_27FEB/__init__.py
@@ -5,17 +5,21 @@
 from abc import ABC, abstractmethod
 
 
+# Parent Class
 class ArrayTransform(ABC):
-
+    # This method should do nothing, it is an abstract method which much be implemented
+    # via other classes
     @abstractmethod
     def transform(self, user_array):
         pass
 
 
+# Child Class
 class Truncate(ArrayTransform):
     def __init__(self, length):
         self._length = length
 
+    # This method fully implements the transform abstract method inherited from parent class
     def transform(self, user_array):
         new_array = []
         i = 0
